f702f072b8a69df4edf0fba42b9d57ff1ab99ae3
Dan Tra <dltra@fcps.edu>
2021-04-13 13:55:38 -0400

    Draw background, Instantiate Sprite, Process Sprite movement, Draw Sprite

--- app/src/main/java/com/example/animatedgamewithfragment/DrawView.java
+++ app/src/main/java/com/example/animatedgamewithfragment/DrawView.java
@@ -10,15 +10,26 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class DrawView extends View {
-
+    Paint p;
+    Sprite sprite;
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        sprite = new Sprite();//instantiate Sprite here, to access screen dims later
+        p = new Paint();
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        invalidate();
+        p.setColor(Color.GRAY);//set paint to gray
+        canvas.drawRect(getLeft(),getTop(),getRight(),getBottom(),p);//paint background gray
+        sprite.update();//process sprite movement and status checks and changes
+        sprite.draw(canvas);//sprite draws itself
+        invalidate();//redraws screen, invokes onDraw()
     }
 }
